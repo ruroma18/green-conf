0c00fce23725d9dca3a85540996bd4c65e560ecb
Roman Rudenko <ruroma18@gmail.com>
2022-02-16 12:27:54 +0200

    Add header nav, bg-image

--- php/header.php
+++ php/header.php
@@ -4,7 +4,20 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="/css/style.css">
+  <link rel="stylesheet" href="./../css/style.css">
   <title>Green conference <?php echo $title; ?></title>
 </head>
 <body>
+  <header class="header-container">
+    <nav class="header-nav">
+      <ul class="nav-list">
+        <li class="nav-list-item">
+          <a href="#" class="nav-link">Registration</a></li>
+        <li class="nav-list-item">
+          <a href="#" class="nav-link">Participants</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main class="main-container">
+
+  </main>
